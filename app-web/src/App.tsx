f111dc96aa0dbf56a9eb95f9b55c5c34800a1df2
rectangle0123 <rectangle0123@gmail.com>
2023-09-10 11:40:41 +0900

    app-webの実装完了

--- app-web/src/App.tsx
+++ app-web/src/App.tsx
@@ -6,12 +6,15 @@ import Blog from './blog/Blog';
 import PrivacyPolicy from './pages/PrivacyPolicy';
 import Error404 from './pages/Error404';
 
-export const router = createBrowserRouter([
-  {path: '*', element: <Error404 />},
-  {path: 'web/sample', element: <Blog />},
-  {path: 'web/privacypolicy', element: <PrivacyPolicy />},
-  {path: 'web/privacypolicy/:lang', element: <PrivacyPolicy />},
-]);
+export const router = createBrowserRouter([{
+  path: process.env.PUBLIC_URL,
+  children: [
+    {path: '*', element: <Error404 />},
+    {path: 'sample', element: <Blog />},
+    {path: 'privacypolicy', element: <PrivacyPolicy />},
+    {path: 'privacypolicy/:lang', element: <PrivacyPolicy />},
+  ],
+}]);
 
 function App() {
   return (
